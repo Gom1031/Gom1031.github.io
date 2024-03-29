00e600b354644d5a89f5e6fc4fd5ceccb67a9ed9
Gom1031 <atheniald@gmail.com>
2024-03-29 19:17:24 +0900

    gitignore 수정

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,38 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1711706887',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/page.min.js'
+    ],allowHosts: [
+      
+        'chirpy-img.netlify.app',
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
